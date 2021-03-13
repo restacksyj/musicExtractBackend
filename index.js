477bfafb0303662f54e4da0823f5da68c984e5f8
restacksyj <theandroiddeveloper.yj@gmail.com>
2021-03-14 03:20:19 +0530

    added error handling

--- index.js
+++ index.js
@@ -5,19 +5,21 @@ const axios = require("axios");
 const cors = require("cors")
 const mime = require("mime-types")
 const multer = require('multer');
-
 const sleep = require('util').promisify(setTimeout);
 const fs = require("fs");
 const SpotifyWebApi = require('spotify-web-api-node');
-const { query } = require("express");
+const apiErrorHandler = require('./error/api-error-handler')
+const ApiError = require("./error/ApiError");
 
 const scopes = ['user-read-private', 'user-read-email', "playlist-modify-private"];
 const redirectUri = 'http://localhost:3000/callback';
+const to = require('await-to-js').default;
 
 
 dotenv.config({ path: path.resolve(__dirname + '/process.env') });
 
 
+//Spotify initialization
 const clientId = process.env.SPOTIFY_CLIENT_KEY;
 const secretKey = process.env.SPOTIFY_CLIENT_SECRET;
 
@@ -28,7 +30,7 @@ var spotifyApi = new SpotifyWebApi({
 });
 
 
-
+//multer storage
 const storage = multer.diskStorage({
     destination: './public/uploads/images',
     filename: function (req, file, cb) {
@@ -39,29 +41,27 @@ const storage = multer.diskStorage({
     }
 });
 
-
-
-const app = express()
-
-app.use(cors())
-
-
 const upload = multer({
     storage: storage,
     limits: { fieldSize: 10 * 1024 * 1024 }
 });
 
+// const allowedOrigins = ["http://localhost:3000", "http://localhost:5000"];
 
-const allowedOrigins = ["http://localhost:3000", "http://localhost:5000"];
 
+
+const app = express()
+
+app.use(cors())
 app.use(express.json())
+
 app.use(express.urlencoded({ extended: true }))
+
 
 
 const PORT = 3000;
 const APP_KEY = process.env.APP_KEY
 const azure_url = "https://eastus.api.cognitive.microsoft.com/vision/v3.1/read/analyze"
-
 let tokenExpirationEpoch;
 
 
@@ -69,66 +69,91 @@ let tokenExpirationEpoch;
 app.get("/", (req, res) => res.json({ "hello": "cool" }))
 
 
-app.post("/detectText", upload.single("file"), async (req, res) => {
-    var config = {
-        headers: {
-            "Ocp-Apim-Subscription-Key": process.env.APP_KEY,
-            "Content-Type": "application/octet-stream"
-        },
+app.post("/detectText", upload.single("file"), async (req, res, next) => {
+    let err, ocrData, finalAnalyzedData;
 
-    };
-
-
-    const dataToPrint = await readAndGetRes(req.file.path)
-    const finalAnalyzedData = printRecText(dataToPrint)
-
-    let uriArr = [];
-    let generatedName = finalAnalyzedData.length > 1 ? `${finalAnalyzedData[0].split(req.body.separator)[0]},${finalAnalyzedData[1].split(req.body.separator)[0]}and friends` : `${finalAnalyzedData[0].split(req.body.separator)[0]} song`
-
-    for (song of finalAnalyzedData) {
-        let eachSong = song.split(req.body.separator)
-        let artistName, songName;
-  
-
-        if (req.body.leftSide === "Artist") {
-            artistName = eachSong[0]
-            songName = eachSong[1]
-        } else {
-            artistName = eachSong[1]
-            songName = eachSong[0]
+    try {
+        const headers = {
+            "Authorization": `Bearer ${spotifyApi.getAccessToken()}`,
+            "Content-Type": "application/json"
         }
 
-        let searchQuery = encodeURI(`track:${songName}+artist:${artistName}`)
-        console.log(searchQuery)
+        if (req.file === undefined || null) throw new ApiError(400, "No file present");
+        [err, ocrData] = await to(readAndGetRes(req.file.path));
 
-        let searchres = await axios.get(`https://api.spotify.com/v1/search?q=${searchQuery}&type=track`, {
-            headers: {
-                "Authorization": `Bearer ${spotifyApi.getAccessToken()}`,
-                "Content-Type": "application/json"
+        if (err) throw new ApiError(err.code, err.message);
+
+        if (!err) {
+
+            finalAnalyzedData = printRecText(ocrData)
+        }
+
+        let uriArr = [];
+        let generatedName = finalAnalyzedData.length > 1 ? `${finalAnalyzedData[0].split(req.body.separator)[0]},${finalAnalyzedData[1].split(req.body.separator)[0]}and friends` : `${finalAnalyzedData[0].split(req.body.separator)[0]} song`
+        let searchres, searcherr;
+        for (song of finalAnalyzedData) {
+
+            let eachSong = song.split(req.body.separator)
+            let artistName, songName;
+
+
+            if (req.body.leftSide === "Artist") {
+                artistName = eachSong[0]
+                songName = eachSong[1]
+            } else {
+                artistName = eachSong[1]
+                songName = eachSong[0]
             }
-        })
 
-        let songsResponse = searchres.data.tracks.items;
-        if (songsResponse.length > 0) {
-            uriArr.push(songsResponse[0].uri)
+            let searchQuery = encodeURI(`track:${songName}+artist:${artistName}`)
+
+
+            try {
+                [searcherr, searchres] = await to(axios.get(`https://api.spotify.com/v1/search?q=${searchQuery}&type=track`, {
+                    headers: headers
+                }))
+            } catch (error) {
+                console.log('error')
+            }
+
+            if (searcherr) throw new ApiError(searcherr.response.status, searcherr.response.statusText)
+
+            let songsResponse = searchres.data.tracks.items;
+            if (songsResponse.length > 0) {
+                uriArr.push(songsResponse[0].uri)
+            }
+
         }
+
+
+        let playlistName = req.body.playlistName == "" ? generatedName : req.body.playlistName;
+
+      
+            const [createErr, makePlaylist] = await to(spotifyApi.createPlaylist(playlistName, { 'public': false }))
+            if (createErr) throw new ApiError(createErr.response.status, createErr.response.statusText)
+
+            const [addErr, addSongsToPlaylist] = await to(spotifyApi.addTracksToPlaylist(makePlaylist.body.id, uriArr))
+            if (addErr) throw new ApiError(createErr.response.status, createErr.response.statusText)
+
+            const [playListUrlErr, playListUrl] = await to(spotifyApi.getPlaylist(makePlaylist.body.id))
+            if (playListUrlErr) throw new ApiError(createErr.response.status, createErr.response.statusText)
+
+            res.send({ "data": { "url": playListUrl.body.external_urls.spotify, "name": playListUrl.body.name } })
+
 
     }
 
-    let playlistName = req.body.playlistName == "" ? generatedName : req.body.playlistName;
-
-
-    const makePlaylist = await spotifyApi.createPlaylist(playlistName, { 'public': false })
-    const addSongsToPlaylist = await spotifyApi.addTracksToPlaylist(makePlaylist.body.id, uriArr)
-    const playListUrl = await spotifyApi.getPlaylist(makePlaylist.body.id)
-    console.log(playListUrl.body)
-
-    res.send({ "data": { "url": playListUrl.body.external_urls.spotify, "name": playListUrl.body.name } })
+    catch (err) {
+        console.log(err)
+        next(err);
+        return;
+    }
 
 })
 
 app.get("/spotifyLogin", (req, res) => {
     res.redirect(spotifyApi.createAuthorizeURL(scopes))
+
 });
 
 
@@ -147,11 +172,8 @@ app.get("/callback", (req, res) => {
 
     spotifyApi.authorizationCodeGrant(code).then(
         function (data) {
-            // Set the access token and refresh token
             spotifyApi.setAccessToken(data.body['access_token']);
             spotifyApi.setRefreshToken(data.body['refresh_token']);
-
-            // Save the amount of seconds until the access token expired
             tokenExpirationEpoch =
                 new Date().getTime() / 1000 + data.body['expires_in'];
             console.log(
@@ -174,7 +196,7 @@ app.get("/callback", (req, res) => {
 
 let numberOfTimesUpdated = 0;
 
-setInterval(function () {
+const tokenFn = () => setInterval(function () {
     console.log(
         'Time left: ' +
         Math.floor(tokenExpirationEpoch - new Date().getTime() / 1000) +
@@ -194,6 +216,7 @@ setInterval(function () {
                 );
             },
             function (err) {
+                next(ApiError.refreshToken("Not authorized"))
                 console.log('Could not refresh the token!', err.message);
             }
         );
@@ -205,7 +228,6 @@ setInterval(function () {
 
 
 const readAndGetRes = async (filePath) => {
-
     var config = {
         headers: {
             "Ocp-Apim-Subscription-Key": process.env.APP_KEY,
@@ -213,35 +235,49 @@ const readAndGetRes = async (filePath) => {
         },
 
     };
-    let fileStream = fs.createReadStream(filePath);
-    let resp = await axios.post(azure_url, fileStream, config)
-    fs.promises.unlink(filePath)
 
-    let operationUrl = resp.headers['operation-location'];
-    while (true) {
+    try {
+        let resp, err;
+        let fileStream = fs.createReadStream(filePath);
+        [err, resp] = await to(axios.post(azure_url, fileStream, config))
+        fs.promises.unlink(filePath)
 
-        resp = await axios.get(operationUrl, config)
-        if (resp.data.status === "succeeded") {
-            console.log("success")
-            break;
+        if (err) throw new ApiError(err.response.status, err.response.data.error.message)
+
+        if (resp.status === 202) {
+            let operationUrl = resp.headers['operation-location'];
+            while (true) {
+
+                resp = await axios.get(operationUrl, config)
+                if (resp.data.status === "succeeded") {
+                    console.log("success")
+                    break;
+                }
+                if (resp.status === "failed") {
+
+                    console.log("failed")
+                    throw ApiError.internal("Failed,something went wrong")
+
+                }
+                await sleep(1000);
+
+            }
+            return resp.data.analyzeResult.readResults;
+
         }
-        if (resp.status === "failed") {
-            console.log("failed")
-            break;
-        }
-        await sleep(1000);
+
+    } catch (e) {
+        console.log(e)
+        throw new ApiError(e.code, e.message)
 
     }
 
 
-    return resp.data.analyzeResult.readResults;
+
+
+
 
 }
-
-
-
-
-
 
 
 const printRecText = (readResults) => {
@@ -256,7 +292,6 @@ const printRecText = (readResults) => {
                 mappedData.push(line.words.map(w => w.text).join(' '));
 
             }
-            console.log(mappedData)
             return mappedData;
 
         }
@@ -267,157 +302,6 @@ const printRecText = (readResults) => {
 }
 
 
-
-
-
-
 app.listen(PORT, () => console.log(`Server running on port ${APP_KEY}`));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const express = require("express")
-// const dotenv = require("dotenv")
-// const path = require('path');
-// const axios = require("axios");
-// const cors = require("cors")
-// const { parse, stringify } = require('flatted');
-
-
-
-// const { config } = require("process");
-// const { read } = require("fs");
-// const { waitForDebugger } = require("inspector");
-// dotenv.config({ path: path.resolve(__dirname + '/process.env') });
-
-
-// const app = express()
-
-
-// const allowedOrigins = ["http://localhost:3000", "http://localhost:5000"];
-
-// app.use(
-//     cors({
-//         origin: function (origin, callback) {
-//             if (!origin) return callback(null, true);
-//             if (allowedOrigins.indexOf(origin) === -1) {
-//                 var msg =
-//                     "The CORS policy for this site does not " +
-//                     "allow access from the specified Origin.";
-//                 return callback(new Error(msg), false);
-//             }
-//             return callback(null, true);
-//         }
-//     })
-// );
-// app.use(express.json())
-
-
-// const PORT = 3000;
-// const APP_KEY = process.env.APP_KEY
-
-// const azure_url = "https://eastus.api.cognitive.microsoft.com/vision/v3.0-preview/read/analyze"
-// // const analyzeResults = "https://eastus.api.cognitive.microsoft.com/vision/v3.0-preview/read/analyzeResults/"
-
-// app.get("/", (req, res) => res.json({ "hello": "cool" }))
-
-// app.post("/detectText", async (req, res) => {
-
-//     var config = {
-//         headers: { 'Content-Type': 'application/json', "Ocp-Apim-Subscription-Key": process.env.APP_KEY },
-
-//     };
-//     console.log(req.body)
-//     // console.log(req.body)
-//     try {
-//         const resp = await axios.post(azure_url, req.body, config)
-//         console.log(resp)
-//         const readRes = await getFinalRes(resp.headers['operation-location'], res);
-//         // const final =  printRecText(readRes, res)
-//         // console.log(final)
-//         // console.log(printRecText(readRes, res))
-//         const fin = printRecText(readRes)
-//         res.send({ "data": fin })
-//     } catch (err) {
-//         console.log(err.data);
-//     }
-
-// })
-
-
-// // app.get("/results",(req,res) => {
-
-// // })
-
-// const getFinalRes = async (header, res) => {
-//     var config = {
-//         headers: { 'Content-Type': 'application/json', "Ocp-Apim-Subscription-Key": process.env.APP_KEY },
-
-//     };
-//     let response = await axios.get(header, config)
-
-//     while (response["status"] !== "succeeded") {
-//         await sleep(1500)
-//         response = await response.data;
-
-//     }
-//     // console.log(`"readResults":${response.analyzeResul}`)
-//     return response.analyzeResult;
-// }
-
-// function sleep(ms) {
-//     return new Promise((resolve) => {
-//         setTimeout(resolve, ms);
-//     });
-// }
-
-
-// const printRecText = (readResults) => {
-//     console.log(`"readResults":${readResults}`)
-//     let mappedData = ""
-
-//     readResults = readResults.readResults
-//     for (const page in readResults) {
-//         // console.log(page);
-//         // if (readResults.length > 1) {
-//         //     console.log(`==== Page: ${page}`);
-//         // }
-//         const result = readResults[page];
-//         if (result.lines.length) {
-//             for (const line of result.lines) {
-//                 mappedData += line.words.map(w => w.text).join(' ');
-
-//                 // res.send("hello")
-//                 // res.send({ "data": line.words.map(w => w.text).join(' ')})
-//             }
-//             return mappedData;
-
-//         }
-//         else {
-//             console.log('No recognized text.');
-//             return mappedData;
-//         }
-//     }
-// }
-
-
-
-
-
-
-// app.listen(PORT, () => console.log(`Server running on port ${APP_KEY}`));
+app.use(apiErrorHandler);
